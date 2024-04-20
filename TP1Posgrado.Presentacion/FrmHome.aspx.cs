d623f2f6a0bf4d152923fd0b98f1a8b9b61bb158
Julián <julian.martin.93@gmail.com>
2024-04-20 11:15:03 -0300

    Agregar archivos de proyecto.

---
+++ TP1Posgrado.Presentacion/FrmHome.aspx.cs
@@ -0,0 +1,15 @@
+﻿using TP1Posgrado.ModeloDatos;
+using System;
+
+namespace TP1Posgrado.Presentacion
+{
+    public partial class FrmHome : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            EntidadesDataDataContext db = new EntidadesDataDataContext();
+            GridPRODUCTO.DataSource = db.Producto;
+            GridPRODUCTO.DataBind();
+        }
+    }
+}
